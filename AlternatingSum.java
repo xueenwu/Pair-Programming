e3609b082d4a64845ff5d6a63beab6e5b7132435
xueenwu <45607584+xueenwu@users.noreply.github.com>
2019-01-03 15:59:03 -0500

    Working with String
    it takes the string and changes it

---
+++ AlternatingSum.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class AlternatingSum 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		int[] numbers = parseString(input);
+		int sum = alternateSum(numbers);
+		System.out.println(sum);
+		sc.close();
+	}
+	
+	public static int alternateSum(int[] intArray)
+	{
+		int sum = 0;
+		for (int i = 0; i < intArray.length; i++)
+		{
+			if (i % 2 == 0)
+			{
+				sum += intArray[i];
+			}	
+			else
+			{
+				sum -= intArray[i];
+			}
+		}
+		return sum;
+	}
+	
+	public static int[] parseString(String input)
+	{
+		input = reduce(input);
+		int doneIndex = input.indexOf("done");
+		input = input.substring(0, doneIndex);
+		int count = countSpaces(input);
+		int[] numbers = new int[count];
+		int space;
+		for (int i = 0; i < count; i++)
+		{
+			space = input.indexOf(" ");
+			numbers[i] = Integer.parseInt(input.substring(0, space));
+			input = input.substring(space + 1);
+		}
+		return numbers;
+	}
+	
+	public static int countSpaces(String str)
+	{
+		int count = 0;
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (str.charAt(i) == ' ')
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static String reduce(String str)
+	{
+		String reduced = "";
+		for (int i = 0; i < str.length(); i++) 
+		{
+			if (str.charAt(i) != ' ' || i == str.length() - 1 
+					|| str.charAt(i + 1) != ' ')
+			{
+				reduced += str.substring(i, i + 1);
+			}
+		}
+		return reduced;
+	}
+}
